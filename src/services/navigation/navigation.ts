530c6f57ec726e8cc18119ac4064360e49e742d0
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-19 12:04:50 +0200

    Navigation -> Multi layered navigation, finally.

--- src/services/navigation/navigation.ts
+++ src/services/navigation/navigation.ts
@@ -4,7 +4,7 @@ import { Language } from "@services/utils/language/language";
 import type { Page } from "@decorators";
 import type { State } from "@services/state/state";
 import { StateKeys } from "@constants/stateKeys.constant";
-import { IPages, IPage } from "./types";
+import { IPage, IPages, IPagesTree } from "./types";
 
 export class Navigation {
     private loader = new Loader({});
@@ -12,12 +12,9 @@ export class Navigation {
     private cachedPages: Map<string, Page<any>> = new Map();
     private i18n = new Language();
     private history: string[] = [];
+    public tree: IPagesTree;
 
-    get pages(): IterableIterator<string> {
-        return this._pages.keys();
-    }
-
-    constructor(private state: State, private ref: HTMLElement, private _pages: IPages, private homePage = '/home', public basePath = '/') {
+    constructor(private state: State, private ref: HTMLElement, private pages: IPages, private homePage = '/home', public basePath = '/') {
         window.addEventListener('popstate', () => {
             if (location.hash.length) return;
             this.history[this.history.length - 2]
@@ -25,6 +22,7 @@ export class Navigation {
                 : this.fisrtLoad();
         });
         window.addEventListener('hashchange', () => history.replaceState(null, '', window.location.pathname));
+        this.tree = this.createTree();
         this.subscribes();
     }
 
@@ -36,6 +34,16 @@ export class Navigation {
         this.state.subscribe(StateKeys.navigate, (path) => this.loadingProcess(path));
         // Load Page.
         this.state.subscribe(`${this.basePath}:${StateKeys.contentReady}`, () => this.ref.replaceChild(this.currentPage, this.loader));
+    }
+
+    private createTree(pages: IPages = this.pages): IPagesTree {
+        const tree: IPagesTree = [];
+        pages.forEach((value, key) => {
+            value.getPages
+                ? tree.push({ [key]: this.createTree(value.getPages()) })
+                : tree.push(key);
+        });
+        return tree;
     }
 
     // ------------------------------
@@ -71,6 +79,7 @@ export class Navigation {
     private loadingProcess(path: string): void {
         if (path.slice(1).remove('-') === this.currentPage?.id) return;
         // this.log('loadingProcess', path);
+        path = this.searchFullPath(path) || this.homePage;
         this.pushState(path);
         try {
             this.ref.replaceChild(this.loader, this.currentPage);
@@ -82,14 +91,14 @@ export class Navigation {
 
     private navigationLogic(path: string): void {
         path = this.findPage(path);
-        // this.log('navigationLogic', `${this.getBasePath()}${path}`);
+        // this.log('navigationLogic', path);
         document.title = `Vanilla | ${(path).slice(1).addSpaces('-').titleCase()}`;
         if (this.cachedPages.has(path)) {
             this.currentPage = this.cachedPages.get(path)!;
             this.ref.replaceChild(this.currentPage, this.loader);
             if (this.currentPage.navigation) this.currentPage.navigation.reload();
         } else {
-            const Page = this.getPage(path);
+            const Page = this.pages.get(path)!;
             if (Page.name === this.currentPage?.constructor.name && !location.pathname.includes(path)) return;
             const texts = this.i18n.getTexts(path.remove(/(\-|\/)/));
             this.cachedPages.set(path, new Page(texts, this.state));
@@ -100,36 +109,34 @@ export class Navigation {
     // ------------------------------
     // Utilities.
     // ------------------------------
-    private getPage(path: string): IPage {
-        // this.log('getPage', path);
-        return this._pages.get(path) as IPage;
+    private searchFullPath(path: string, tree = this.tree): string | null {
+        // this.log('searchPath', path);
+        const fullPath: string[] = [];
+        for (const branch of tree) {
+            if (typeof branch !== 'string')
+                for (const key of Object.keys(branch)) {
+                    const result = this.searchFullPath(path, branch[key]);
+                    if (result) fullPath.push(key, result);
+                }
+            else if (branch === path) fullPath.push(path);
+        }
+        return fullPath.length ? fullPath.join('') : null;
     }
 
     private findPage(path: string): string {
         // this.log('findPage', path);
         if (path === this.basePath) return this.homePage;
         const pathArr = path.slice(1).split('/');
-        for (let i = 0; i < pathArr.length; i++) if (this._pages.has(`/${pathArr[i]}`)) return `/${pathArr[i]}`;
+        for (let i = 0; i < pathArr.length; i++) if (this.pages.has(`/${pathArr[i]}`)) return `/${pathArr[i]}`;
         return this.homePage;
     }
 
     private pushState(path: string): void {
         this.history.push(path);
-        window.history.pushState(null, '', `${this.getBasePath()}${path}`);
-    }
-
-    private getBasePath(): string {
-        return this.basePath === '/' ? '' : this.basePath;
+        window.history.pushState(null, '', `${this.basePath === '/' ? '' : this.basePath}${path}`);
     }
 
     private log(fnName: string, path: string): void {
         console.log('base path: ', this.basePath, `\n${fnName}: `, path, '\n-----------------------------');
     }
-
-    // ------------------------------
-    // Statics.
-    // ------------------------------
-    // static lastCrumb(): string {
-    //     return location.pathname.slice(1).split('/').last;
-    // }
 }
