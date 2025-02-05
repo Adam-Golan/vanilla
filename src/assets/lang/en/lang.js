e0c12ba680a798e9173a441bde1c9ff7b0626c37
Adam-Golan <slowmoon.adam@gmail.com>
2025-02-05 14:16:48 +0200

    Erasing lang from assets.

--- src/assets/lang/en/lang.js
+++
@@ -1,8 +0,0 @@
-import { home } from './home/home';
-import { contactus } from './contact-us/contact-us';
-import { aboutus } from './about-us/about-us';
-export const texts = {
-    home,
-    contactus,
-    aboutus
-};
