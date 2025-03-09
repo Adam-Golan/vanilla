6be141b0a5ca436d588514f104c8535187d469e2
Adam-Golan <slowmoon.adam@gmail.com>
2025-03-09 15:12:35 +0200

    Radio -> Implementing cAlot, using label as container.

--- src/app/shared/modules/form/components/radio/radio.ts
+++ src/app/shared/modules/form/components/radio/radio.ts
@@ -9,18 +9,18 @@ export class Radio extends FormComponent<IRadioProps> {
     declare field: HTMLInputElement;
     protected createMe(): HTMLFieldSetElement {
         this.field = this.cElem('input');
-        const fieldset = this.cElem('fieldset'), legend = this.cElem('legend'), children: HTMLElement[] = [legend];
+        const [fieldset, legend] = this.cAlot([{ tag: 'fieldset' }, { tag: 'legend' }]), children: HTMLElement[] = [legend];
         fieldset.className = 'form-group';
         legend.innerHTML = this.props.label;
         for (const radio of this.props.values) {
-            const div = this.cElem('div'), inp = this.cElem('input'), label = this.cElem('label');
+            const [label, inp, span] = this.cAlot([{ tag: 'label' }, { tag: 'input' }, { tag: 'span' }]);
             inp.type = 'radio';
             inp.name = this.props.name;
-            inp.id = inp.value = label.htmlFor = label.innerText = radio;
+            inp.id = inp.value = label.htmlFor = span.innerText = radio;
             if (this.props.value === radio) inp.checked = true;
             inp.oninput = () => inp.checked ? this.onInput(inp.value) : null;
-            div.append(inp, label);
-            children.push(div);
+            label.append(inp, span);
+            children.push(label);
         }
         fieldset.append(...children);
         return fieldset;
