02fdf2edb98c886f29bfc439d18d6b36c565a7e8
Adam-Golan <Adam-Golan@users.noreply.github.com>
2025-05-05 08:32:31 +0000

    lang.mjs -> Adding rmSync import.

---
+++ lang/en/lang.js
@@ -0,0 +1,8 @@
+import { home } from './home/home';
+import { contactus } from './contact-us/contact-us';
+import { aboutus } from './about-us/about-us';
+export const texts = {
+    home,
+    contactus,
+    aboutus
+};
