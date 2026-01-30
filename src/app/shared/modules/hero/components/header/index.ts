a2ee7e30cb325abc1d01881428984a39aeadfb4a
Adam-Golan <slowmoon.adam@gmail.com>
2026-01-30 10:26:39 +0200

    Header -> Changed innerText to innerHTML

--- src/app/shared/modules/hero/components/header/index.ts
+++ src/app/shared/modules/hero/components/header/index.ts
@@ -6,10 +6,9 @@ import './header.scss';
 @ComponentDecorator
 export class Header extends Component<IHeaderText> {
     protected init(): void {
-        const h1 = this.cElem('h1');
-        const h3 = this.cElem('h3');
-        h1.innerText = this.config.header;
-        h3.innerText = this.config.subHeader;
-        this.append(h1, h3);
+        const [header, subHeader] = this.cAlot([{ tag: 'h1' }, { tag: 'h3' }]);
+        header.innerHTML = this.config.header;
+        subHeader.innerHTML = this.config.subHeader;
+        this.append(header, subHeader);
     }
 }
